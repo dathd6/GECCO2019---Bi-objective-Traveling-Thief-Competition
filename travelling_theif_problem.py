3261b0a2582b55248e4168f5078c1bb63260d55f
Dat Hoang <dathd6@Dats-MacBook-Air.local>
2023-11-27 00:34:54 +0000

    [Duc Dat Hoang]: Add crowding distance and non-dominated sorting, Fix replacement function

--- travelling_theif_problem.py
+++ travelling_theif_problem.py
@@ -12,7 +12,6 @@ class TTP:
 
         self.travelling_time = self.calc_fitness_travelling_time() 
         self.total_profit = self.calc_fitness_total_profit()
-        self.fitness = self.objective_function()
     
     # Task 19: Compare solution
     def __gt__(self, other):
@@ -25,8 +24,8 @@ class TTP:
                (self.total_profit == other.total_profit)
     def __ge__(self, other):
         # Weakly dominance
-        return (self.total_profit == other.total_profit and self.travelling_time < other.travelling_time) or \
-               (self.travelling_time == other.travelling_time and self.total_profit > other.total_point)
+        return (self.total_profit >= other.total_profit and self.travelling_time < other.travelling_time) or \
+               (self.travelling_time <= other.travelling_time and self.total_profit > other.total_point)
     
     # Task 5: Calculate weight at city i #
     def cal_weight_at_city(self, i):
